af1246f8126680fa48405ce9776eb19a43aece2c
ayushgosi <anime.ayush@gmail.com>
2017-06-25 04:01:06 +0530

    facebook and twitter sign in setup

--- src/Data/services/auth.service.js
+++ src/Data/services/auth.service.js
@@ -2,28 +2,42 @@ import firebase from 'firebase';
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyBVStbSMdSQabhlGQbxRJ5S1n0tMGOzJkg",
-  authDomain: "waylo-coding-challenge.firebaseapp.com",
-  databaseURL: "https://waylo-coding-challenge.firebaseio.com",
-  projectId: "waylo-coding-challenge",
-  storageBucket: "",
-  messagingSenderId: "358369695785"
+   apiKey: "AIzaSyBVStbSMdSQabhlGQbxRJ5S1n0tMGOzJkg",
+   authDomain: "waylo-coding-challenge.firebaseapp.com",
+   databaseURL: "https://waylo-coding-challenge.firebaseio.com",
+   projectId: "waylo-coding-challenge",
+   storageBucket: "",
+   messagingSenderId: "358369695785"
 };
 firebase.initializeApp(config);
 
-var Auth = firebase.auth;
-var GithubProvider = new firebase.auth.GithubAuthProvider();
-var FacebookProvider = new firebase.auth.FacebookAuthProvider();
-
 export default function AuthService() {
-  "ngInject";
+   "ngInject";
 
-  var service = this;
+   var service = this;
+   var firebaseauth = firebase.auth();
 
-  // firebase objects
-  let Auth = firebase.auth();
-  let GithubProvider = new firebase.auth.GithubAuthProvider();
-  let FacebookProvider = new firebase.auth.FacebookAuthProvider();
+   service.oauthSignIn = (provider) => {
+      let Provider = provider + 'AuthProvider';
+      let ProviderObj = new firebase.auth[Provider]();
+      let signin = firebaseauth.signInWithPopup(ProviderObj);
+      signin.then(function(result) {
+         let user = result.user;
+         return user;
+      })
+      .catch(function(error) {
+         return error;
+      });
 
+      return signin;
+   };
+
+   service.userLogin = () => {
+
+   };
+
+   service.userSignup = () => {
+
+   };
 
 }
--- src/main/main.controller.js
+++ src/main/main.controller.js
@@ -1,6 +1,24 @@
-export default function MainController() {
-  "ngInject";
+export default function MainController(AuthService) {
+   "ngInject";
+   var main = this;
 
-  var main = this;
-  console.log('into main');
+   main.oauthSignIn = (provider) => {
+      let oauthlogin = AuthService.oauthSignIn(provider);
+      oauthlogin.then(function(result) {
+         console.log('User Signed In using ' + provider);
+         console.log(result);
+         // $state.go('home');
+      })
+      .catch(function(error) {
+         console.log('error');
+      });
+   };
+
+   main.userLogin = (user) => {
+
+   };
+
+   main.userSignup = (user) => {
+
+   };
 }
