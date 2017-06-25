a1f5db0022ca6dd9642881f3b27e3d1a44624a9b
ayushgosi <anime.ayush@gmail.com>
2017-06-25 16:35:22 +0530

    login progress-bar added

--- src/main/main.controller.js
+++ src/main/main.controller.js
@@ -2,7 +2,13 @@ export default function MainController(AuthService, $state) {
    "ngInject";
    var main = this;
 
+   main.isLoading = false;
+   let toggleLoadingFlag = () => {
+      main.isLoading = !main.isLoading;
+   };
+
    main.oauthSignIn = (provider) => {
+      toggleLoadingFlag();
       let oauthlogin = AuthService.oauthSignIn(provider);
       oauthlogin.then(function(result) {
          console.log('User Signed In using ' + provider);
@@ -14,23 +20,29 @@ export default function MainController(AuthService, $state) {
    };
 
    main.userLogin = (user) => {
+      toggleLoadingFlag();
       let userlogin = AuthService.userLogin(user)
       userlogin.then(function(result) {
          console.log(result);
+         toggleLoadingFlag();
          $state.go('home');
       })
       .catch(function(error) {
+         toggleLoadingFlag();
          console.log(error);
       });
    };
 
    main.userSignup = (user) => {
+      toggleLoadingFlag();
       let userSignup = AuthService.userSignup(user)
       userSignup.then(function(result) {
          console.log(result);
+         toggleLoadingFlag();
          $state.go('home');
       })
       .catch(function(error) {
+         toggleLoadingFlag();
          console.log(error);
       });
    };
